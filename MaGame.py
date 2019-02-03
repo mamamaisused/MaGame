df333264c894d2cd6f0b00f8dd0bbfd1cc363648
MAMAMAisused <vin_mlc@126.com>
2019-02-03 21:09:17 +0800

    增加了ImageRole的图像尺寸设置。做了个Demo移动角色，但是似乎用blitImage的方式移动对象效率一般，有卡顿。

--- MaGame.py
+++ MaGame.py
@@ -2,10 +2,13 @@
 import pygame
 from pygame.locals import *
 import MaColor
+import os.path
 
 global SCREEN
 global FONT
 global CLOCK
+global Role1
+global DIR
 class MaGame():
     def __init__(self,SIZE_X = 800,SIZE_Y = 600,_color = (255,255,255)):
         global SCREEN,FONT
@@ -26,17 +29,20 @@ class MaGame():
             _font = FONT
         imgText = FONT.render(text,True,self.FontColor)
         SCREEN.blit(imgText,location_x_y)
-        pygame.display.update()
+        #pygame.display.update()
 
 
 mygame = MaGame(800,600,MaColor.pink)
 
 def initialize():
     #变量初始化放这里
-    global FONT,CLOCK
+    global FONT,CLOCK,Role1,DIR
     fontsize = 40
+    DIR = os.path.split(os.path.abspath(__file__))[0]
     FONT = pygame.font.Font(None,fontsize)
     CLOCK = pygame.time.Clock()#设置帧率
+    Role1 = ImgRole("s.png")
+    Role1.SetScale(0.5)
     pass
     #end of initialize
 
@@ -44,19 +50,60 @@ def ClearScreen():
     #清屏
     global SCREEN
     SCREEN.fill(mygame.BackColor)
-    pygame.display.update()
+    #pygame.display.update()
+    #end of ClearSCreen
 
 def FrameTask():
     #每帧的任务
-    global CLOCK
-    #Frame = 40 fps
-    CLOCK.tick(40)   
+    global CLOCK,Role1,role1_x,role1_y,speed
+    #Frame = 40 fps   
+    CLOCK.tick(60)
+    ClearScreen()    
+    Role1.Show((role1_x,role1_y))
+    mygame.GamePrint("Happy New Year!",(role1_x-50,role1_y-50))
+    if role1_x>500 :
+        speed = -speed
+    if role1_x<50:
+        speed = -speed
+    #end of FrameTask
+
+#基于图像的角色，后续扩展继续Sprite的角色
+class ImgRole():
+    costumes = []
+    def __init__(self,imgpath):
+        global DIR
+        imgpath = os.path.join(DIR, imgpath)
+        print(imgpath)
+        self.costume = pygame.image.load(imgpath).convert_alpha()
+        self.costumes = [self.costume]
+        #角色尺寸
+        self.Width,self.Height = self.costume.get_size()
+    def AppendCostume(self,imgpath):
+        global DIR
+        imgpath = os.path.join(DIR, imgpath)
+        surf = pygame.image.load(imgpath).convert_alpha()
+        self.costumes.append(surf)
+    def Show(self,_position,index = 0):
+        global SCREEN
+        SCREEN.blit(self.costumes[index],_position)
+        #pygame.display.update()
+    def SetScale(self,_scale):
+        for i in range(len(self.costumes)):
+            _width = int(_scale*self.Width)
+            _height = int(_scale*self.Height)
+            self.costumes[i] = pygame.transform.smoothscale(self.costumes[i],(_width,_height))
+
+role1_x = 100
+role1_y = 300
+speed = 1
 
 def main():
-    global SCREEN
+    global SCREEN,Role1,speed,role1_x
     initialize()
     mygame.GamePrint("Hello, World!",(100,100))
-    while True:
+    while True:  
+        role1_x+=speed
+        pygame.display.update()      
         FrameTask()
         for event in pygame.event.get():
             #事件捕捉          
