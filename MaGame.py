565e7badcdcbfa546e16cb79c4ea178725db9f7e
MAMAMAisused <vin_mlc@126.com>
2019-02-19 21:55:17 +0800

    这个工程现在不是用作库建设了，我准备从PacMan开发中重新安排库，这个工程现在是用来随便写测试代码的。目前写了三维旋转变换。

--- MaGame.py
+++ MaGame.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 import MaColor
 import os.path
 import numpy
+import math
 
 global SCREEN
 global FONT
@@ -16,13 +17,15 @@ class MaGame():
     def __init__(self,SIZE_X = 800,SIZE_Y = 600,_color = (255,255,255)):
         global SCREEN,FONT
         pygame.init()
-        SCREEN = pygame.display.set_mode((SIZE_X,SIZE_Y))
+        SCREEN = pygame.display.set_mode((SIZE_X,SIZE_Y),DOUBLEBUF)
+        pygame.display.set_caption('By MAMAMA')
         SCREEN.fill(_color)
         pygame.display.update()
-        FONT = pygame.font.Font(None,40)
+        #FONT = pygame.font.Font(None,40)
+        FONT = pygame.font.SysFont('C:\Windows\Fonts\simhei.ttf',40)
         self.Height = SIZE_Y
         self.Width = SIZE_X
-        self.FontColor = MaColor.black
+        self.FontColor = (255,255,255)
         self.BackColor = _color
     def GamePrint(self,text,location_x_y = None,_font = None):
         global SCREEN,FONT
@@ -30,22 +33,22 @@ class MaGame():
             location_x_y = (self.Width/2,self.Height-50)
         if _font is None:
             _font = FONT
-        imgText = FONT.render(text,True,self.FontColor)
+        imgText = _font.render(text,True,self.FontColor)
         SCREEN.blit(imgText,location_x_y)
         #pygame.display.update()
 
 
-mygame = MaGame(600,600,MaColor.powderblue)
+mygame = MaGame(400,600,MaColor.black)
 
 def initialize():
     #变量初始化放这里
     global FONT,CLOCK,DIR
     global SCREEN_WIDTH,SCREEN_HEIGHT
-    fontsize = 40
+    fontsize = 45
     SCREEN_WIDTH = mygame.Width
     SCREEN_HEIGHT = mygame.Height
     DIR = os.path.split(os.path.abspath(__file__))[0]
-    FONT = pygame.font.Font(None,fontsize)
+    FONT = pygame.font.SysFont('C:\Windows\Fonts\simhei.ttf',fontsize)
     CLOCK = pygame.time.Clock()#设置帧率
     #end of initialize
 
@@ -56,16 +59,90 @@ def ClearScreen():
     #pygame.display.update()
     #end of ClearSCreen
 
+class Point():
+    def __init__(self,_x,_y,_z):
+        self.x = _x
+        self.y = _y
+        self.z = _z
+    def get_flat_pos(self):
+        return (self.x+200,self.y+100)
+    def RotY(self,ang):
+        _x = self.x
+        _y = self.y
+        _z = self.z
+        self.x = _x * math.cos(ang) + _z * math.sin(ang)
+        self.z = _x * (-math.sin(ang)) + _z * math.cos(ang)
+    def RotX(self,ang):
+        _x = self.x
+        _y = self.y
+        _z = self.z
+        self.y = _y * math.cos(ang) + _z * (-math.sin(ang))
+        self.z = _y * math.sin(ang) + _z * math.cos(ang)
+    def RotZ(self,ang):
+        _x = self.x
+        _y = self.y
+        _z = self.z
+        self.x = _x * math.cos(ang) + _y * (-math.sin(ang))
+        self.y = _x * math.sin(ang) + _y * math.cos(ang)
+
+
+Point1 = Point(0,0,0)
+Point2 = Point(100,0,0)
+Point3 = Point(100,100,0)
+Point4 = Point(0,100,0)
+
+Point5 = Point(100,0,100)
+Point6 = Point(100,100,100)
+
+Point7 = Point(0,100,100)
+Point8 = Point(0,0,100)
+
+Point1.RotX(math.pi/6)
+Point2.RotX(math.pi/6)
+Point3.RotX(math.pi/6)
+Point4.RotX(math.pi/6)
+Point5.RotX(math.pi/6)
+Point6.RotX(math.pi/6)
+Point7.RotX(math.pi/6)
+Point8.RotX(math.pi/6)
+
 def FrameTask():
     #每帧的任务
     global CLOCK,SCREEN
     global role1,testgroup
+    global Point1,Point2,Point3,Point4
+    Point1.RotY(math.pi / 60)
+    Point2.RotY(math.pi / 60)
+    Point3.RotY(math.pi / 60)
+    Point4.RotY(math.pi / 60)
+    Point5.RotY(math.pi / 60)
+    Point6.RotY(math.pi / 60)
+    Point7.RotY(math.pi / 60)    
+    Point8.RotY(math.pi / 60)
     #Frame = 40 fps   
     CLOCK.tick(40)
-    role1.rect.move_ip(1,0)
+    if role1.rect.left < 400:
+        role1.rect.move_ip(1,0)
     #when you need to clear the screen
     ClearScreen() 
-    testgroup.draw(SCREEN)  
+    #testgroup.draw(SCREEN) 
+    pygame.draw.lines(SCREEN,MaColor.cyan,True,[Point1.get_flat_pos(),Point2.get_flat_pos(),\
+        Point3.get_flat_pos(),Point4.get_flat_pos()],3) 
+    pygame.draw.lines(SCREEN,MaColor.cyan,True,[Point2.get_flat_pos(),Point5.get_flat_pos(),\
+        Point6.get_flat_pos(),Point3.get_flat_pos()],3) 
+    pygame.draw.lines(SCREEN,MaColor.cyan,True,[Point3.get_flat_pos(),Point6.get_flat_pos(),\
+        Point7.get_flat_pos(),Point4.get_flat_pos()],3)
+    pygame.draw.lines(SCREEN,MaColor.cyan,True,[Point1.get_flat_pos(),Point8.get_flat_pos(),\
+        Point7.get_flat_pos(),Point4.get_flat_pos()],3)
+    pygame.draw.lines(SCREEN,MaColor.cyan,True,[Point1.get_flat_pos(),Point2.get_flat_pos(),\
+        Point5.get_flat_pos(),Point8.get_flat_pos()],3)  
+    pygame.draw.lines(SCREEN,MaColor.cyan,True,[Point7.get_flat_pos(),Point8.get_flat_pos(),\
+        Point5.get_flat_pos(),Point6.get_flat_pos()],3)
+    mygame.FontColor = (255,255,255)
+    mygame.GamePrint('Happy Lantern Festival',(30,230))
+    font = pygame.font.SysFont('Times New Roman',20,True,True)
+    mygame.FontColor = MaColor.powderblue
+    mygame.GamePrint('by MAMAMA',(240,265),font)
     #end of FrameTask
 
 #Based on Sprite
